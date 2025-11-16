c6b69020d8f68518887f486bdcaf9b4f17b7ee58
Sanskar-Rijal <rijalsanskar@gmail.com>
2025-11-16 21:42:50 +0545

    enabling cors

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require("express");
 
+const cors = require("cors");
+
 const morgan = require("morgan");
 
 const AppError = require("./utils/AppError");
@@ -12,6 +14,7 @@ const globalErrorHandler = require("./Controller/errorConroller");
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
